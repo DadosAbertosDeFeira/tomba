c97c7ea87bb20906181c12f0414b04716a0d4677
Ana Paula Gomes <apgomes88@gmail.com>
2021-03-07 11:09:12 +0100

    Corrige caminho para labels do estado

--- tomba/models.py
+++ tomba/models.py
@@ -5,7 +5,7 @@ from spacy.lang.pt import Portuguese
 nlp = Portuguese()
 
 ruler = nlp.add_pipe("entity_ruler")
-patterns = json.load(open("data/states_label.json"))
+patterns = json.load(open("data/states/states_label.json"))
 
 ruler.add_patterns(patterns)
 
