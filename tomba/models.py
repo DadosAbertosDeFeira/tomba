0c9ce4a678c6447a747a66fc91caa9d188d7ca92
Ana Paula Gomes <ana.gomes@nubank.com.br>
2023-01-30 20:39:20 +0100

    Adiciona arquivo de cidades ao ruler

--- tomba/models.py
+++ tomba/models.py
@@ -5,10 +5,10 @@ from spacy.lang.pt import Portuguese
 
 nlp = Portuguese()
 
-ruler = nlp.add_pipe("entity_ruler")
-patterns = json.load(open("data/states/states_label.json"))
+ruler = nlp.add_pipe("entity_ruler").from_disk("data/cities/cities.jsonl")
 
-ruler.add_patterns(patterns)
+state_patterns = json.load(open("data/states/states_label.json"))
+ruler.add_patterns(state_patterns)
 
 Path("models/").mkdir(parents=True, exist_ok=True)
 nlp.to_disk("models/pt_core_news_sm_addresses")
